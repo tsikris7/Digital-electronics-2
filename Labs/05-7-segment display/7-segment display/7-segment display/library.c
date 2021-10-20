df0fb71e56558ab438123ae6a20a61fad5ccf570
student <student@PC-629.urel.feec.vutbr.cz>
2021-10-20 16:44:39 +0200

    [Lab] 05-7-segment display

---
+++ Labs/05-7-segment display/7-segment display/7-segment display/library.c
@@ -0,0 +1,16 @@
+/*
+ * 7-segment display.c
+ *
+ * Created: 20.10.2021 15:20:22
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
